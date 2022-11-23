a640ec27d29a8c094fdbc4d3bac68aeaa0427fb4
Martin <34113899+cooyun4github@users.noreply.github.com>
2022-11-24 00:30:48 +0800

    Update iic.py
    hardware i2c no need manual start() and stop()

--- ov7670/iic.py
+++ ov7670/iic.py
@@ -2,15 +2,9 @@ from machine import Pin, I2C
 
 # class i2c represents a pair of master-slav
 # 1-N need multiplu instances
-# how to use:
-# import ii2
-# i2c = iic.IIC(0, 17, 16)
 class IIC:
     def __init__(self, i2c_id, scl_pin, sda_pin, frequency = 400_000, slv_addr = 0):
-        self.scl_pin = Pin(scl_pin)
-        self.sda_pin = Pin(sda_pin)
         self.i2c = I2C(i2c_id, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=frequency)
-
         if (0 == slv_addr):
             slv_addrs = self.i2c.scan()
             if len(slv_addrs) == 0:
@@ -19,23 +13,9 @@ class IIC:
                 print("i2c device found, address:", hex(slv_addrs[0]))
                 self.slv_addr = slv_addrs[0]
 
+    def write(self, reg, data):
+        self.i2c.writeto(self.slv_addr, bytearray([reg, data]))
+        # print("write ", "{:#04x}".format(data), "to reg", "{:#04x}".format(reg), "success")
 
-    def start(self):
-        self.scl_pin.on()
-        self.sda_pin.on()
-        # time.sleep_us(1)
-        self.sda_pin.off()
-        # time.sleep_us(1)
-
-
-    def stop(self):
-        self.scl_pin.on()
-        self.sda_pin.off()
-        #time.sleep_us(1)
-        self.sda_pin.on()
-
-
-    def write(self, write_sig, data):
-        self.start()
-        self.i2c.writeto(self.slv_addr, bytearray([write_sig, data]))
-        self.stop()
+    def readfrom_mem(self, reg, num):
+        return self.i2c.readfrom(self.slv_addr, reg, num)
